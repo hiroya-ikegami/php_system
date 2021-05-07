867c73905b4c112eb4e84bf15e492b8e53cd9cd3
hi-roya <huroya0712@gmail.com>
2021-05-07 09:58:39 +0900

    pull

--- systemdemo/login/login_index.php
+++ systemdemo/login/login_index.php
@@ -22,7 +22,7 @@ $testuser = "123";
 */
 $dsn='mysql:dbname=task_zoom;host=localhost;charaset=utf8';
 $user='root';
-$password='0305';
+$password='xampppass';
 $dbh = new PDO($dsn,$user,$password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
