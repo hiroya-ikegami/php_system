3f8e56a6f732989f0cf0a057239003f625ea2d0d
mishima2t73 <shima.k7646@gmail.com>
2021-05-07 10:06:32 +0900

    ログアウトのURLとログインが機能しててなかったので確認と修正

--- systemdemo/login/login_index.php
+++ systemdemo/login/login_index.php
@@ -8,43 +8,33 @@ $staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
 
 $staff_pass=md5($staff_pass);
 
-/*
-$testuser = "123";
-    $dsn = 'mysql:dbname=task_zoom;host=localhost;charaset=utf8';
-    $user = 'root';
-    $password = '0305';
-    $dbh = new PDO($dsn,$user,$password);
-    $dbh ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    //$sql = 'INSERT INTO zoom_schedule(name,password) VALUES (?,?)';
-    $testsql = 'INSERT INTO zoom_schedule(title,s_date,start_time,end_time,content,participant,regist_p) VALUES('.$title.",".$calendar.",".$start_time.",".$end_time.",".$content.",".$member.",".$testuser.")";
-    print $testsql;
-    //print ($title.$calendar.$start
-*/
 $dsn='mysql:dbname=task_zoom;host=localhost;charaset=utf8';
 $user='root';
 $password='0305';
 $dbh = new PDO($dsn,$user,$password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sql='SELECT name  FROM user WHERE mail=? AND pass=?';
+//$prsql = 'SELECT name  FROM user WHERE mail='."$staff_mail".' AND pass='."$staff_pass" ;
+
+$sql='SELECT name  FROM user WHERE mail = ? AND pass = ?';
 $stmt=$dbh->prepare($sql);
 $data[]=$staff_mail;
 $data[]=$staff_pass;
 $stmt->execute($data);
-$prsql = 'SELECT name  FROM user WHERE mail='."$staff_mail".' AND pass='."$staff_pass" ;
-print($prsql);
-
+print_r($data);
+print($sql ."<br>");
 $rec=$stmt->fetch(PDO::FETCH_ASSOC);
-print_r($rec);
+var_dump($rec);
+
 $dbh=null;
-if($rec=-false){
+if($rec==false){
     print'メールアドレスかパスワードが間違っています。<br/>';
-    print'<a href="login.php">戻る</a>';
+    print'<a href="login.html">戻る</a>';
 }else{
 session_start();
 $_SESSION['login']=1;
 $_SESSION['staff_mail']=$staff_mail;
-$_SESSION['staff_pass']=$rec['pass'];
+$_SESSION['staff_pass']=$staff_pass;
 print('ok');
     header('Location:../demotop.html');
     exit();
