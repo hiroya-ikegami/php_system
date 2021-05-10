10f5237ca7a0469780f1929d5d26c7c233107527
hi-roya <huroya0712@gmail.com>
2021-05-10 16:46:56 +0900

    登録画面からトップ画面までの遷移及びDBデータ格納ができるようになりました。 DBの名前等は水島さんのほうに合わせていきます。

--- systemdemo/login/success.php
+++ systemdemo/login/success.php
@@ -3,47 +3,52 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
-<title>ログイン成功ページ</title>
+<title>登録完了画面</title>
 </head>
 <body>
 <?php
-//try{
-    $dsn= 'mysql:dbname=system;host=localhost';
-    $user= 'root';
-    $password= '';
-    $dbh= new PDO($dsn);
-    $dbh->query('SET NAMES utf8');
+try{
     $name = $_POST['name'];
     $birth = $_POST['birth'];
     $mail = $_POST['mail'];
-    $pass = $_POST['pass'];
+    $pass = $_POST['pass']; 
 
     $name=htmlspecialchars($name);
     $birth=htmlspecialchars($birth);
     $mail=htmlspecialchars($mail);
     $pass=htmlspecialchars($pass);
 
+    $dsn= 'mysql:dbname=system;host=localhost';
+    $user= 'root';
+    $password= '0305';
+    $dbh= new PDO($dsn,$user,$password);
+    $dbh ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    //$dbh->query('SET NAMES utf8');
+    $subsql='INSERT INTO member(name,birth,mail,pass)VALUES("'.$name.'","'.$birth.'","'.$mail.'","'.$pass.'")';
+    $sql='INSERT INTO member(name,birth,mail,pass)VALUES(?,?,?,?)';//"'.$name.'","'.$birth.'","'.$mail.'","'.$pass.'"
+    $stmt=$dbh->prepare($sql);
+    $data[] = $name;
+    $data[] = $birth;
+    $data[] = $mail;
+    $data[] = $pass;
+    $stmt ->execute($data);
+
+    $dbh=null;
+
     print $name;
     print '様<br/>';
     print 'ご登録ありがとうございます。<br/>';
-    print '<form method="post"action="demotop.html">';
-    print '<input type="submit"value="ホームへ">';
-    print '</form>';
-    $sql='INSERT INTO member(name,birth,mail,pass)VALUES("'.$name.'","'.$birth.'","'.$mail.'","'.$pass.'")';
-    $stmt=$dbh->prepare($sql);
-    $stmt->execute();
+    print "<a href='../demotop.html'>トップ画面へ</a>";
 
-    $dbh=null;
-//}
-  //  catch(Exception $e)
-//{
+}
+  catch(Exception $e)
+{
 	print 'ただいま障害により大変ご迷惑をお掛けしております。<br/>';
     print '登録画面に戻る。</br>';
     print '<form method="post"acttion="demo_index.html">';
     print '<input type="submit"value="登録画面へ">';
     print '</form>';
-
-//}
+}
 ?>
 </body>
 </html>
