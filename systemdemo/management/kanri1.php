e7e60a98bf61865d14d18e6544018b044af71dcd
hi-roya <huroya0712@gmail.com>
2021-05-12 17:46:24 +0900

    管理画面1途中 ですが一応あげておきます

--- systemdemo/management/kanri1.php
+++ systemdemo/management/kanri1.php
@@ -7,13 +7,39 @@
 
 </head>
 <body bgcolor="#fbdac8">
+<table>
+<tr bgcolor="#AAAAAA">
+<th>名前</th>
+<th>誕生日</th>
+<th>メール</th>
+<th>パスワード</th>
+</tr>
 <?php
 $dbhost="localhost";
 $dbuser="root";
 $dbpass="0305";
 $dbnamae="task_zoom"
 
-$sql_com=mysql_connect($dbhost,$dbuser,$dbpass);
+mysql_connect('loalhost','root','0305','task_zoom');
+if($db->connect_error){
+	echo $dbh->connect_error;
+	exit();
+}
+$sql='SELECT*FROM member';
+while($data=mysql_fetch_array($result)){
+  //echo'<p>'.$data['id'].':'.$data['name'].':'.$data['birth'].':'.$data['mail'].':'.$data['pass']."</p>\n";
+  echo '<tr>';
+  echo '<td>'.$data['name'].'</td>';
+  echo '<td>'.$data['birth'].'</td>';
+  echo '<td>'.$data['mail'].'</td>';
+  echo '<td>'.$data['pass'].'</td>';
+  echo '</tr>';
+}
+$db=mysql_close($db);
+if(!$db){
+	exit('データベースとの接続を閉じられませんでした。');
+}
+/*$sql_com=mysql_connect($dbhost,$dbuser,$dbpass);
 mysql_select_db($dbname,$sql_con);
 
 $create_sql="CREATE TABLE IF NOT EXISTS'TABLE'("
@@ -26,7 +52,20 @@ $create_sql="CREATE TABLE IF NOT EXISTS'TABLE'("
 mysql_query($create_sql,$sql_com); //テーブル作成
 
 $ret_array=array();//適当な配列に入れる
-$select_sql="select*form''"
+$select_sql="select*form'task_zoom'"
+
+if($res_select=mysql_query($select_sql,$sql_con)){
+	while($line_res_select=mysql_fetch_assoc($res_select)){
+		$ret_array[]=array(
+			'id'=>$line_res_select['ID'],
+			'name'=>$line_res_select['NAME'],
+			'text'=>$line_res_select['TEXT'],
+			'date'=>$line_res_select['DATE'],
+		);
+	}
+}
+mysql_close($sql_con);
+*/
 ?>
 
 <div id="page">
@@ -45,7 +84,7 @@ $select_sql="select*form''"
 
 <th1> </th1>
 
-<th>		</th><th>		</th><th>		</th><th>   </th>
+<th><?php print "$"; ?></th><th>		</th><th>		</th><th>   </th>
 
 </tr>
 
